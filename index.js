f5ecfde836cb1340a172c3da76274986f81538f3
Nestor Masinde <nestormasinde@gmail.com>
2023-12-18 21:37:32 +0300

    Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array){
+   const winningGame = array.find(item =>item.result === 'W');
+   if (winningGame){
+    return winningGame.year;
+   }else{
+    return undefined;
+   }
+   }
+
+
+//  The find() method takes a callback function as its argument, which should return a boolean based on condition.
